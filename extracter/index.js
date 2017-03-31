4fa9a64e820622f781778a884823fbaee7820f28
Vignesh S <vignesh6993@live.in>
2017-03-31 15:11:12 +0530

    Deleted unwanted files

--- extracter/index.js
+++
@@ -1,9 +0,0 @@
-var striptags = require('striptags')
-var fs = require('fs')
-
-fs.readFile('cities.html', 'utf8', function (err, data) {
-  if (err) {
-    return console.log(err)
-  }
-  console.log(striptags(data))
-})
